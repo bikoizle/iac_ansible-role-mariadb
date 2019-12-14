70dfc1eb53b9f69249c273657ced433436d4eb34
bikoizle <cmartinoo@uoc.edu>
2019-12-14 16:49:10 +0100

    Added extra blank line to tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,6 +4,7 @@ def test_mariadb_is_working(host):
     cmd = host.run("mysql -e 'SHOW DATABASES;' -u root --password=insecure")
     assert cmd.rc == 0
 
+
 def test_mariadb_test_user_is_gone(host):
     cmd = host.run("mysql -e 'SHOW DATABASES;'")
     assert cmd.rc == 1
